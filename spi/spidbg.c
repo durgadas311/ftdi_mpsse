5bfd84a39930776d15f6e63eae9e607904946c3a
Douglas Miller <durgadas311@gmail.com>
2021-03-01 17:12:21 -0600

    redefine -l value, add -v and minimize normal output, remove WREN code/option.

--- spi/spidbg.c
+++ spi/spidbg.c
@@ -13,29 +13,22 @@
 #include "ftd2xx.h"
 #include "spilib.h"
 
-#undef USE_WREN
-
 int main(int argc, char **argv) {
-	int len;
+	int tot;
+	int cmd;
+	int len = 0;
 	int port = 0;
+	int verbose = 0;
 	unsigned char *bufo;
 	unsigned char *bufi;
 	int x;
 	int c;
 	FT_HANDLE ft;
-#ifdef USE_WREN
-	int wren = 0;
-	unsigned char wbuf[] = { 0x06 };
-	unsigned char wxbuf[1];
-	unsigned char sbuf[] = { 0x05, 0xff };
-	unsigned char sxbuf[2];
-#endif
 
 	extern char *optarg;
 	extern int optind;
 
-	len = -1;
-	while ((c = getopt(argc, argv, "l:p:w")) != EOF) {
+	while ((c = getopt(argc, argv, "l:p:v")) != EOF) {
 		switch(c) {
 		case 'l':
 			len = strtol(optarg, NULL, 0);
@@ -43,76 +36,49 @@ int main(int argc, char **argv) {
 		case 'p':
 			port = strtol(optarg, NULL, 0);
 			break;
-#ifdef USE_WREN
-		case 'w':
-			wren = 1;
+		case 'v':
+			verbose = 1;
 			break;
-#endif
 		default:
 			fprintf(stderr, "Unknown option '%c'\n", c);
 			exit(1);
 		}
 	}
 	if (argc - optind < 1) {
-		fprintf(stderr, "Usage: %s [-p port][-l len] <byte>[...]\n", argv[0]);
-		fprintf(stderr, "Where 'len' must be at least <byte>[...] length\n");
+		fprintf(stderr, "Usage: %s [-v][-p port][-l len] <byte>[...]\n", argv[0]);
+		fprintf(stderr, "Where 'len' is added to <byte>[...] length (read len)\n");
 		exit(1);
 	}
-	if (len <= 0) {
-		len = argc - optind;
-	}
-	bufo = malloc(len);
-	bufi = malloc(len);
+	cmd = argc - optind;
+	tot = cmd + len;
+	bufo = malloc(tot);
+	bufi = malloc(tot);
 	if (bufo == NULL || bufi == NULL) {
-		fprintf(stderr, "Out of memory, 2x%d bytes\n", len);
+		fprintf(stderr, "Out of memory, 2x%d bytes\n", tot);
 		exit(1);
 	}
-	for (x = 0; x < len; ++x) {
-		if (optind + x < argc) {
-			bufo[x] = (unsigned char)strtol(argv[optind + x], NULL, 0);
-		} else {
-			bufo[x] = (unsigned char)0xff;
-		}
+	for (x = 0; x < cmd; ++x) {
+		bufo[x] = (unsigned char)strtol(argv[optind + x], NULL, 0);
+	}
+	if (len > 0) {
+		// Read data is sent as FF...
+		memset(bufo + cmd, 0xff, len);
 	}
 	ft = spi_open(port);
 	if (ft == NULL) {
 		fprintf(stderr, "Unable to open device, error = %d\n", ftStatus);
 		exit(1);
 	}
-#ifdef USE_WREN
-	if (wren) {
-		x = spi_xfer(ft, wbuf, wxbuf, sizeof(wbuf));
-		if (x < 0) {
-			fprintf(stderr, "Failure during WREN, error = %d\n", ftStatus);
-		}
-	}
-#endif
-	x = spi_xfer(ft, bufo, bufi, len);
+	x = spi_xfer(ft, bufo, bufi, tot);
 	if (x < 0) {
 		fprintf(stderr, "Failure during transfer, error = %d\n", ftStatus);
-	} else {
-#ifdef USE_WREN
-		if (wren) {
-			x = spi_xfer(ft, sbuf, sxbuf, sizeof(sbuf));
-			if (x < 0) {
-				fprintf(stderr, "Failure during READ STATUS, error = %d\n", ftStatus);
-			}
-			printf("WREN:\n");
-			dump_buf(wbuf, sizeof(wbuf));
-			dump_buf(wxbuf, sizeof(wxbuf));
-		}
-#endif
+	} else if (verbose) {
 		printf("Write:\n");
-		dump_buf(bufo, len);
+		dump_buf(bufo, tot);
 		printf("Read (%06x %d):\n", driverVersion, ftStatus);
-		dump_buf(bufi, len);
-#ifdef USE_WREN
-		if (wren) {
-			printf("STATUS:\n");
-			dump_buf(sbuf, sizeof(sbuf));
-			dump_buf(sxbuf, sizeof(sxbuf));
-		}
-#endif
+		dump_buf(bufi, tot);
+	} else if (len > 0) {
+		dump_buf(bufi + cmd, len);
 	}
 	spi_close(ft);
 	return 0;
